47c213e7b4638fdc0aacb91aa49c89a340ed2b26
Steven <steven.r987@gmail.com>
2021-10-14 14:45:22 -0600

    intro

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <p>Understanding risk vs reward is important because it is the basis for most decision making in investing.
+            </p>
+        </div>
+    )
+}
+
+export default Intro;
