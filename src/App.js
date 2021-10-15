ab0a8d8aefb65e9a4bf60eb76c83c731d97a0fcb
Steven <steven.r987@gmail.com>
2021-10-15 15:23:09 -0600

    first example
    $5 -> 6.25 or nothing risk vs reward and probability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
+import Example1 from './components/Example1';
 import Intro from './components/Intro'
+/*
+Example #1: Would you risk $5 for a 80% chance of a $6.25 return? numbers are placeholders
+  Outcomes: risk averse (wouldn't) vs risk tolerant (would) -> explain why in a vaccum such a bet would be worthwhile
+
+Example #2: Short term risk (trading) vs long term risk (investing)
+  Outcomes: 
+*/
 function App() {
   return (
     <div className="App">
@@ -7,6 +15,7 @@ function App() {
         <h1>Risk vs Reward</h1>
       </header>
       <Intro />
+      <Example1 />
     </div>
   );
 }
